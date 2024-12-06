1d83fd908053b5e15ebf482ebe58ad5c654dd9c4
gigcomm <lenovo27835@gmail.com>
2024-12-06 23:14:27 +0300

    upd models

--- backend/django_backend/Anonymous_question/models.py
+++ backend/django_backend/Anonymous_question/models.py
@@ -4,21 +4,18 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 import uuid
 
 
-# Кастомная модель пользователя
+# Модель пользователя
 class User(AbstractUser):
+    # Переопределите поля, чтобы избежать конфликта имен с группами и правами
     groups = models.ManyToManyField(
         Group,
-        related_name="custom_user_set",  # Уникальное имя для обратной ссылки
+        related_name='custom_user_set',  # Уникальное имя для обратной связи
         blank=True,
-        help_text="Группы, к которым принадлежит пользователь.",
-        verbose_name="группы"
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name="custom_user_permissions_set",  # Уникальное имя для обратной ссылки
+        related_name='custom_user_permissions',  # Уникальное имя для обратной связи
         blank=True,
-        help_text="Разрешения, принадлежащие пользователю.",
-        verbose_name="разрешения"
     )
 
     def __str__(self):
@@ -31,11 +28,30 @@ class Test(models.Model):
     description = models.TextField(default="Тестирование")
     сreated_at = models.DateTimeField(auto_now_add=True)  # Дата создания
     is_active = models.BooleanField(default=True)  # Активен ли тест
-    is_anonymous = models.BooleanField(default=False)  # Флаг анонимности
+    is_anonymous = models.BooleanField(default=True)  # Флаг анонимности
+    default_time_limit = models.PositiveIntegerField(default=30,
+                                                     help_text="Default time limit for each question in seconds.")
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tests')  # Создатель теста
 
     def __str__(self):
         return self.title
+
+
+# Модель участника
+class AnonymousParticipant(models.Model):
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Уникальный идентификатор
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='anonymous_participants')  # Связь с тестом)
+
+    def __str__(self):
+        return f"Anonymous ({self.identifier}) -> {self.test.title}"
+
+
+class AuthenticatedParticipant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authenticated_participants')
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='test_participations')  # Связь с тестом)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.test.title}"
 
 
 # Модель вопроса
@@ -63,11 +79,26 @@ class ParticipantAnswer(models.Model):
     selected_option = models.ForeignKey(Answer, on_delete=models.CASCADE,
                                         related_name='chosen_answers')  # Выбранный ответ
     answered_at = models.DateTimeField(auto_now_add=True)  # Время ответа
-    participant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')  # Участник, дающий ответ
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='participant_answers')  # Вопрос
 
+    anonymous_participant = models.ForeignKey(
+        AnonymousParticipant,
+        on_delete=models.CASCADE,
+        related_name='answers',
+        null=True,
+        blank=True
+    )
+    authenticated_participant = models.ForeignKey(
+        AuthenticatedParticipant,
+        on_delete=models.CASCADE,
+        related_name='answers',
+        null=True,
+        blank=True
+    )
+
     def __str__(self):
-        return f"{self.participant.username} -> {self.question.text[:50]} -> {self.selected_option.text}"
+        participant = self.anonymous_participant or self.authenticated_participant
+        return f"{participant} -> {self.question.text[:50]} -> {self.selected_option.text}"
 
 
 # Модель резульатата теста
@@ -76,24 +107,25 @@ class TestResult(models.Model):
     total_questions = models.IntegerField(default=0)  # Всего вопросов
     completed = models.DateTimeField(auto_now_add=True)  # Дата завершения
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='test_results')  # Тест
-    participant = models.ForeignKey(
-        User,
+
+    anonymous_participant = models.ForeignKey(
+        AnonymousParticipant,
         on_delete=models.CASCADE,
         related_name='test_results',
         null=True,
         blank=True  # Поле можно оставить пустым для анонимных участников
     )
-    participant_identifier = models.CharField(
-        max_length=255,
+    authenticated_participant = models.ForeignKey(
+        AuthenticatedParticipant,
+        on_delete=models.CASCADE,
+        related_name='test_results',
         null=True,
         blank=True,
-        help_text="Идентификатор участника для анонимных тестов (например, сессия)."
     )
 
     def __str__(self):
-        if self.test.is_anonymous:
-            return f"Anonymous -> {self.test.title} -> {self.correct_answers}/{self.total_questions}"
-        return f"{self.participant.username} -> {self.test.title} -> {self.correct_answers}/{self.total_questions}"
+        participant = self.anonymous_participant or self.authenticated_participant
+        return f"{participant} -> {self.correct_answers}/{self.total_questions}"
 
 
 # Модель генерирование ссылки для теста
@@ -101,6 +133,11 @@ class TestLink(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     link = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='links')
+
+    def save(self, *args, **kwargs):
+        if not self.test.is_active:
+            raise ValueError("Не удается создать ссылку для неактивного теста")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.link)
