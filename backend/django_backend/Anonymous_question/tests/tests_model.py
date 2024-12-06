cf2b9277a3b0a889e58fca1dc03b7da9eff93124
gigcomm <lenovo27835@gmail.com>
2024-12-06 23:13:56 +0300

    upd tests

--- backend/django_backend/Anonymous_question/tests/tests_model.py
+++ backend/django_backend/Anonymous_question/tests/tests_model.py
@@ -1,8 +1,9 @@
 from django.utils.timezone import now
 
 from django.test import TestCase
-from anonymous_question.models import User, Test, Question, Answer, ParticipantAnswer, TestResult
-import pytest
+from anonymous_question.models import User, Test, Question, Answer, ParticipantAnswer, TestResult, AnonymousParticipant, \
+    AuthenticatedParticipant
+
 
 class ModelTestCase(TestCase):
     def setUp(self):
@@ -12,7 +13,8 @@ class ModelTestCase(TestCase):
             description='Test description',
             сreated_at=now(),
             is_active=True,
-            is_anonymous=False,
+            is_anonymous=True,
+            default_time_limit=30,
             creator=self.user,
         )
         self.question = Question.objects.create(
@@ -22,9 +24,11 @@ class ModelTestCase(TestCase):
         )
         self.answer = Answer.objects.create(
             text='4',
-            is_correct=False,
+            is_correct=True,
             question=self.question,
         )
+        self.anonymous_participant = AnonymousParticipant.objects.create(test=self.test)
+        self.authenticated_participant = AuthenticatedParticipant.objects.create(user=self.user, test=self.test)
 
     def test_creation(self):
         self.assertEqual(Test.objects.count(), 1)
@@ -32,6 +36,15 @@ class ModelTestCase(TestCase):
         self.assertEqual(self.test.creator, self.user)
         self.assertEqual(self.test.description, 'Test description')
         self.assertEqual(self.test.is_active, True)
+        self.assertEqual(self.test.is_anonymous, True)
+        self.assertEqual(self.test.default_time_limit, 30)
+
+    def test_anonymous_participant_creation(self):
+        self.assertEqual(self.anonymous_participant.test, self.test)
+
+    def test_authenticated_participant_creation(self):
+        self.assertEqual(self.authenticated_participant.user, self.user)
+        self.assertEqual(self.authenticated_participant.test, self.test)
 
     def test_question_creation(self):
         self.assertEqual(self.question.text, 'What is 2+2?')
@@ -40,30 +53,39 @@ class ModelTestCase(TestCase):
 
     def test_answer_creation(self):
         self.assertEqual(self.answer.text, '4')
-        self.assertEqual(self.answer.is_correct, False)
+        self.assertEqual(self.answer.is_correct, True)
         self.assertEqual(self.answer.question, self.question)
 
     def test_participants_answer_creation(self):
         participant_answer = ParticipantAnswer.objects.create(
             selected_option=self.answer,
-            participant=self.user,
+            anonymous_participant=self.anonymous_participant,
             question=self.question,
         )
         self.assertEqual(participant_answer.selected_option, self.answer)
-        self.assertEqual(participant_answer.participant, self.user)
+        self.assertEqual(participant_answer.anonymous_participant, self.anonymous_participant)
         self.assertEqual(participant_answer.question, self.question)
 
-    def test_test_result_creation(self):
+    def test_test_result_creation_for_anonymous_participant(self):
         test_result = TestResult.objects.create(
             correct_answers=1,
             total_questions=10,
             test=self.test,
-            participant=self.user,
+            anonymous_participant=self.anonymous_participant,
         )
         self.assertEqual(test_result.correct_answers, 1)
         self.assertEqual(test_result.total_questions, 10)
         self.assertEqual(test_result.test, self.test)
+        self.assertEqual(test_result.anonymous_participant, self.anonymous_participant)
 
-
-
-
+    def test_test_result_creation_for_authenticated_participant(self):
+        test_result = TestResult.objects.create(
+            correct_answers=5,
+            total_questions=10,
+            test=self.test,
+            authenticated_participant=self.authenticated_participant,
+        )
+        self.assertEqual(test_result.correct_answers, 5)
+        self.assertEqual(test_result.total_questions, 10)
+        self.assertEqual(test_result.test, self.test)
+        self.assertEqual(test_result.authenticated_participant, self.authenticated_participant)
