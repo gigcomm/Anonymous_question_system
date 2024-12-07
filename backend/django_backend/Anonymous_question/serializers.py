56ecba3fb5509307e91f6929b90da7845692ece7
gigcomm <lenovo27835@gmail.com>
2024-12-08 00:11:04 +0300

    upd serializers

--- backend/django_backend/Anonymous_question/serializers.py
+++ backend/django_backend/Anonymous_question/serializers.py
@@ -1,35 +1,54 @@
 from rest_framework import serializers
-from .models  import*
+from .models import *
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Question
-        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email']
+
 
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
         fields = '__all__'
 
+
+class AnonymousParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnonymousParticipant
+        fields = '__all__'
+
+
+class AuthenticatedParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthenticatedParticipant
+        fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = '__all__'
+
 
 class ParticipantAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParticipantAnswer
         fields = '__all__'
 
+
 class TestResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestResult
         fields = '__all__'
+
 
 class TestLinkSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/django_backend/Anonymous_question/views.py
+++ backend/django_backend/Anonymous_question/views.py
@@ -1,39 +1,56 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, generics
 from .models import *
-from .serializers import*
+from .serializers import *
 
 
+# Представление для получения списка вопросов и создания нового вопроса
 
-#Представление для получения списка вопросов и создания нового вопроса
-class QuestionAPIList(generics.ListCreateAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
 
 class UserAPIList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class TestAPIList(generics.ListCreateAPIView):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
+
+class AnonymousParticipantView(generics.ListCreateAPIView):
+    queryset = AnonymousParticipant.objects.all()
+    serializer_class = AnonymousParticipantSerializer
+
+
+class AuthenticatedParticipantView(generics.ListCreateAPIView):
+    queryset = AuthenticatedParticipant.objects.all()
+    serializer_class = AuthenticatedParticipantSerializer
+
+
+class QuestionAPIList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
 
 class AnswerAPIList(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
 
+
 class ParticipantAnswerAPIList(generics.ListCreateAPIView):
     queryset = ParticipantAnswer.objects.all()
     serializer_class = ParticipantAnswerSerializer
+
 
 class TestResultAPIList(generics.ListCreateAPIView):
     queryset = TestResult.objects.all()
     serializer_class = TestResultSerializer
 
+
 class TestLinkAPIList(generics.ListCreateAPIView):
     queryset = TestLink.objects.all()
     serializer_class = TestLinkSerializer
-
