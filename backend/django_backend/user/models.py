4fbb61f870fe81a7c7da841db90dbd46375a3bb3
gigcomm <lenovo27835@gmail.com>
2024-12-08 22:46:15 +0300

    upd models

--- backend/django_backend/user/models.py
+++ backend/django_backend/user/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 
 
-
 # Модель пользователя
 class User(AbstractUser):
 
@@ -13,8 +12,7 @@ class User(AbstractUser):
 # Модель участника
 class AnonymousParticipant(models.Model):
     identifier = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Уникальный идентификатор
-    from test_management.models import Test
-    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='anonymous_participants')  # Связь с тестом)
+    test = models.ForeignKey('test_management.Test', on_delete=models.CASCADE, related_name='anonymous_participants')  # Связь с тестом)
 
     def __str__(self):
         return f"Anonymous ({self.identifier}) -> {self.test.title}"
@@ -22,8 +20,7 @@ class AnonymousParticipant(models.Model):
 
 class AuthenticatedParticipant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authenticated_participants')
-    from test_management.models import Test
-    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='test_participations')  # Связь с тестом)
+    test = models.ForeignKey('test_management.Test', on_delete=models.CASCADE, related_name='test_participations')  # Связь с тестом)
 
     def __str__(self):
         return f"{self.user.username} -> {self.test.title}"
