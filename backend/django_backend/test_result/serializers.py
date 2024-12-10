bd318f05def6bdcc25bfed5645c0c4256672a689
gigcomm <lenovo27835@gmail.com>
2024-12-10 11:27:42 +0300

    upd serializers

--- backend/django_backend/test_result/serializers.py
+++ backend/django_backend/test_result/serializers.py
@@ -12,3 +12,10 @@ class ParticipantAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParticipantAnswer
         fields = '__all__'
+
+    def validate(self, data):
+        if data.get('anonymous_participant') and data.get('authenticated_participant'):
+            raise serializers.ValidationError("Only one participant type can be specified.")
+        if not data.get('anonymous_participant') and not data.get('authenticated_participant'):
+            raise serializers.ValidationError("One participant type must be specified.")
+        return data
