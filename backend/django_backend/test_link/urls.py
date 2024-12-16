3f5859d15da815e2101abcbcbd736866b464c602
gigcomm <lenovo27835@gmail.com>
2024-12-16 18:19:29 +0300

    upd: Allows users to access specific tests through unique links

--- backend/django_backend/test_link/urls.py
+++ backend/django_backend/test_link/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+
 
 from test_link.views import *
 
 urlpatterns = [
     path('', TestLinkAPIList.as_view(), name='test-link-list'),
     path('<int:pk>/', TestLinkDelUpdView.as_view(), name='test-link-del-upd'),
+    path('<uuid:link>/', TestLinkView.as_view(), name='test-link-view'),
     ]
